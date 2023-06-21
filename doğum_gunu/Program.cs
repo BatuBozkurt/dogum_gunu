5579ead16dbfdb560122989033fc680ac4aab301
sudenazcivelek <sudenazzcivelek@gmail.com>
2023-06-22 01:48:29 +0300

    Add project files.

---
+++ doğum_gunu/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("Doğum tarihinizi giriniz: ");
+DateTime dogumtarihi = Convert.ToDateTime(Console.ReadLine());
+DateTime buguntarih = DateTime.Now;
+TimeSpan zamanaraligi = buguntarih.Subtract(dogumtarihi);
+Console.WriteLine(zamanaraligi.TotalDays / 365);
+Console.ReadKey();
